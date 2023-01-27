575f4d9875ca4b6cc1ee7277e3c96555dc4bad7c
sbojorge <sara.bojorge@gmail.com>
2023-01-27 10:37:31 +0000

    Creat Post method and display the result in a modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,47 @@ function displayStatus(data) {
     resultsModal.show();
 
 }
+
+// Make a POST request
+
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+                        method: "POST",
+                        headers: {
+                                    "Authorization": API_KEY,
+                    },
+                        body: form,
+            });
+    
+    const data = await response.json();
+
+    if(response.ok) {
+           console.logcl(data);
+    } else {
+            throw new Error(data.error);
+        };
+
+}
+
+function displayErrors(data) {
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
+    if (data.total_errors === 0) {
+            results = `<div class="no_errors">No errors reported!</div>`;
+        } else {
+            results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+            for (let error of data.error_list) {
+                results += `<div>At line <span class="line">${error.line}</span>, `;
+                results += `column <span class="column">${error.col}:</span></div>`;
+                results += `<div class="error">${error.error}</div>`;
+            }
+        }
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
